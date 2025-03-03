f97b26694ab2f6ee0ee7b3b82eb118f9a06aacbf
unknown <jbniyikiza20@gmail.com>
2025-03-03 16:01:42 +0200

    3rd March commit

--- src/Page/Home/Hero.js
+++ src/Page/Home/Hero.js
@@ -10,17 +10,12 @@ export default function Hero() {
 
   return (
     <section className="welcome-section heroSection" id="heroSection">
-      {/* Background Video */}
       <video className="background-video" autoPlay loop muted>
         <source src="./img/video.mp4" type="video/mp4" />
         <source src="./img/video.webm" type="video/webm" />
         Your browser does not support the video tag.
       </video>
-
-      {/* Overlay */}
       <div className="overlay"></div>
-
-      {/* Text Content */}
       <div className="text-content">
         <h2>Hello, I am <span className="myName">Jean Baptiste N.</span></h2>
         <h2>A <span> Full Stack</span> Web Developer</h2>
